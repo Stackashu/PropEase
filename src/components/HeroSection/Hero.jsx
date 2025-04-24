7cbfad071d57d123d3ae27135e63a430857202d1
Stackashu <ashu12141214@gmail.com>
2025-04-24 14:43:25 +0530

    add Hero Section

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import './Hero.css'
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='hero-out'>
+      <div className="hero-inndiv">
+       
+       
+        <h1 className='first-head'><span>S</span>eamless <span>P</span>roperty  <br/><span>S</span>olutions,<span>S</span>implified</h1>
+        <div>
+        <h1 className='second-head'>Propease Reality Pvt Ltd.</h1>
+        </div>
+
+      </div>
     </div>
   )
 }
